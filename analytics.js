dda110b37037094406d6f1573276f2cfa229c417
YamanoItsuki <42466955+ituyama@users.noreply.github.com>
2023-06-16 14:14:26 +0900

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,33 +1,80 @@
-let events = [];
+const testId = 'test1'; // テストIDによってテストを特定します
 
-async function trackEvent(eventType, id) {
-    let event = {
-        url: window.location.href, // Add this line
-        time: new Date(),
-        eventType: eventType,
-        id: id
-    };
-    events.push(event);
-
-    // Send the event to your server
-    let response = await fetch('https://your-api-url.com/track-event', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(event)
-    });
-
+function fetchTestInfo() {
+  return fetch(`/get-test-info?testId=${testId}`).then(response => {
     if (!response.ok) {
-        console.error('Error sending event to server:', response.statusText);
+      throw new Error('Network response was not ok');
     }
+    return response.json();
+  });
 }
 
-document.querySelectorAll('[id]').forEach((element) => {
-    element.addEventListener('mouseover', function() {
-        trackEvent('mouseover', this.id);
+function updateDOM(html) {
+  // ここにDOMを更新するためのコードを書いてください
+  // 例：document.body.innerHTML = html;
+}
+
+function trackEvent(eventData) {
+  return fetch('/track-event', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  });
+}
+
+function handleEvent(eventType, elementId) {
+  trackEvent({
+    timestamp: new Date().toISOString(),
+    version: selectedVersion,
+    userAgent: navigator.userAgent,
+    eventType: eventType,
+    eventData: {
+      elementId: elementId,
+    },
+  });
+}
+
+let selectedVersion;
+
+fetchTestInfo().then(testInfo => {
+  if (testInfo && testInfo.abTestStatus === 'running') {
+    const randomNum = Math.random(); // 0と1の間のランダムな数字を生成します
+
+    if (randomNum < 0.5) {
+      selectedVersion = 'A';
+      // A案（デフォルト）を選択した場合、何もしないでください
+    } else {
+      selectedVersion = 'B';
+      // B案を選択した場合、DOMを更新します
+      updateDOM(testInfo.versionBHTML);
+    }
+
+    // ログをFlaskアプリケーション経由で送信します
+    trackEvent({
+      timestamp: new Date().toISOString(),
+      version: selectedVersion,
+      userAgent: navigator.userAgent,
+      eventType: 'pageView',
+      eventData: { /* ここにイベントデータを追加します */ }
+    }).then(() => console.log('Event tracked successfully'))
+      .catch(error => console.error('Error tracking event:', error));
+
+    // すべての要素についてイベントリスナーを設定します
+    const elements = document.querySelectorAll('[id]');
+    elements.forEach(element => {
+      element.addEventListener('click', () => handleEvent('click', element.id));
+      element.addEventListener('mouseover', () => handleEvent('mouseover', element.id));
+      // 表示イベントのトラッキング
+      if (element.offsetParent !== null) { // offsetParent is null when the element is not visible
+        handleEvent('view', element.id);
+      }
     });
-    element.addEventListener('click', function() {
-        trackEvent('click', this.id);
-    });
+  }
 });
